3790931e1a8516b1a456c6449ddd8b5ee782f5ec
Task1 done <s4egol@DESKTOP-6D3H9BK>
2016-10-19 13:15:22 +0300

    Task1 done

---
+++ Task1UI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Task1UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 8, 3, 17, 10, 9, 1 };
+            QuickSort.Sorting(arr, 0, arr.Length - 1);
+
+            foreach (var element in arr)
+            {
+                Console.Write(element);
+                Console.Write(" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
